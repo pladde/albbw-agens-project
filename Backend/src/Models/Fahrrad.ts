a3f3cee385854298d677826fda85438c6ac5a50c
pladde <mpoerschke@proton.me>
2025-12-09 10:11:19 +0100

    Konstruktor hinzugefügt, Attribute verkürzt, Getter und Setter gesetzt

--- Backend/src/Models/Fahrrad.ts
+++ Backend/src/Models/Fahrrad.ts
@@ -1,12 +1,107 @@
 export class Fahrrad 
 {
-    // Attribute
-    id!: number;
-    marke?: string;
-    rahmennummer?: string;
-    besonderheiten?: string;
-    bearbeitungsstatus!: string;
-    erfasstAm!: Date;
-    erfasstVon!: string;
-    herausgegebenAn?: string;
+    //#region Konstruktor
+    constructor(
+        private id?: number, 
+        private marke?: string, 
+        private rahmennummer?: string, 
+        private besonderheiten?: string, 
+        private bearbeitungsstatus?: string,
+        private erfasstAm?: Date,
+        private erfasstVon?: string,
+        private herausgegebenAn?: string)
+        {
+        }
+
+    //#endregion
+
+    //#region Getter und Setter
+    //#region id
+    public getId()
+    {
+        return this.id;
+    }
+    public setId(id: number): void
+    {
+        this.id = id;
+    }
+    //#endregion
+
+    //#region marke
+    public getMarke(): string | undefined
+    {
+        return this.marke;
+    }
+    public setMarke(marke: string): void
+    {
+        this.marke = marke;
+    }
+    //#endregion
+
+    //#region rahmennummer
+    public getRahmennummer(): string | undefined
+    {
+        return this.rahmennummer;
+    }
+    public setRahmennummer(rahmennummer: string): void
+    {
+        this.rahmennummer = rahmennummer;
+    }
+    //#endregion
+
+    //#region besonderheiten
+    public getBesonderheiten(): string | undefined
+    {
+        return this.besonderheiten;
+    }
+    public setBesonderheiten(besonderheiten: string): void
+    {
+        this.besonderheiten = besonderheiten;
+    }
+    //#endregion
+
+    //#region bearbeitungstatus
+    public getBearbeitungsstatus(): string | undefined
+    {
+        return this.bearbeitungsstatus;
+    }
+    public setBearbeitungsstatus(bearbeitungsstatus: string): void
+    {
+        this.bearbeitungsstatus = bearbeitungsstatus;
+    }
+    //#endregion
+
+    //#region erfasstAm
+    public getErfasstAm(): Date | undefined
+    {
+        return this.erfasstAm;
+    }
+    public setErfasstAm(erfasstAm: Date): void
+    {
+        this.erfasstAm = erfasstAm;
+    }
+    //#endregion
+
+    //#region erfasstVon
+    public getErfasstVon(): string | undefined
+    {
+        return this.erfasstVon;
+    }
+    public setErfasstVon(erfasstVon: string): void
+    {
+        this.erfasstVon = erfasstVon;
+    }
+    //#endregion
+
+    //#region herausgegebenAn
+    public getHerausgegebenAn(): string | undefined
+    {
+        return this.herausgegebenAn;
+    }
+    public setHerausgegebenAn(herausgegebenAn: string): void
+    {
+        this.herausgegebenAn = herausgegebenAn;
+    }
+    //#endregion
+    //#endregion
 }
