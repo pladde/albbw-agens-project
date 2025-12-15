a72d535fef10cc14b6a779faca65e554cdcd9679
pladde <mpoerschke@proton.me>
2025-12-15 13:25:40 +0100

    Routen hinzugefügt

--- Backend/src/Routes/index.ts
+++ Backend/src/Routes/index.ts
@@ -12,7 +12,10 @@ router.post('/fahrrad/create', (req, res) => {
 router.get('/fahrrad/readById', (req, res) => {
 })
 
-router.get('/fahrrad/readByRahmennummer', (req, res) => {
+router.get('/fahrrad/readByString', (req, res) => {
+})
+
+router.get('/fahrrad/readByDate', (req, res) => {
 })
 
 router.get('/fahrrad/readAll', (req, res) => {
