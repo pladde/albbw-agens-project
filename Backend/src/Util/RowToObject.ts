3d0f6eaef9cf5e1faadf7cf4e6fd7c3dc1951db4
pladde <mpoerschke@proton.me>
2025-12-15 08:31:22 +0100

    Das Array empfängt jetzt jeweils ein Fahrrad-Objekt

--- Backend/src/Util/RowToObject.ts
+++ Backend/src/Util/RowToObject.ts
@@ -6,17 +6,12 @@ import { Fahrrad } from "../Models/Fahrrad";
 export class RowToObject
 {
     /**
-     * Diese Methode mappt die Daten aus dem übergebenen Objekt in ein Fahrrad-Objekt und gibt es zurück. Falls das übergebene Objekt 
-     * ungültig ist, gibt der Guard NULL zurück.
-     * @param fahrradData Das Objekt aus dem das Fahrrad-Objekt erstellt werden soll.
-     * @returns Fahrrad | null - Ein Fahrrad-Objekt wird zurückgegeben. Wenn der Guard erkennt, dass das übergebene Objekt NULL ist, wird NULL zurückgegeben. 
+     * Diese Methode mappt die Daten aus dem übergebenen Objekt in ein **Fahrrad-Objekt** und gibt es zurück.
+     * @param fahrradData Das Objekt aus dem das **Fahrrad-Objekt** erstellt werden soll.
+     * @returns `Fahrrad` Ein Fahrrad-Objekt wird zurückgegeben. 
      */
-    public mapRowToFahrrad(fahrradData: any): Fahrrad | null
+    public mapRowToFahrrad(fahrradData: any): Fahrrad
     {
-        if (!fahrradData) {
-            return null; // Kein Fahrrad mit dieser ID gefunden
-        }
-
         const fahrrad: Fahrrad = new Fahrrad(
             fahrradData.fahrrad_id,
             fahrradData.fahrradmarke,
