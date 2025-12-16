3fe6fe1e597cb6ea0ddfc8839b544041d9ed2ced
pladde <mpoerschke@proton.me>
2025-12-16 10:08:16 +0100

    Die Klassen zur Dependency-Injection wurden importiert und konfiguriert.

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,4 +1,15 @@
 import express from 'express'
+
+//Import der Abhängigkeiten
+import { FahrradRepository } from './src/Repository/FahrradRepository'
+import { FahrradService } from './src/Service/FahrradService'
+import { FahrradController } from './src/Controller/FahrradController'
+
+//Set-Up der Abhängigkeiten
+const fahrradRepository = new FahrradRepository();
+const fahrradService = new FahrradService(fahrradRepository);
+const fahrradController = new FahrradController(fahrradService);
+
 const app = express()
 const port = 3000
 
