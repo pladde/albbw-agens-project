ba37269a09384abbf9c5ffaafd1aa48267467c64
pladde <mpoerschke@proton.me>
2025-12-17 08:59:58 +0100

    README.md geändert

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,22 +1,24 @@
-import express from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
 
 //Import der Abhängigkeiten
-import { FahrradRepository } from './src/Repository/FahrradRepository'
-import { FahrradService } from './src/Service/FahrradService'
-import { FahrradController } from './src/Controller/FahrradController'
+import { FahrradRepository } from './src/Repository/FahrradRepository.ts';
+import { FahrradService } from './src/Service/FahrradService.ts';
+import { FahrradController } from './src/Controller/FahrradController.ts';
 
 //Set-Up der Abhängigkeiten
 const fahrradRepository = new FahrradRepository();
 const fahrradService = new FahrradService(fahrradRepository);
 const fahrradController = new FahrradController(fahrradService);
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/api', (req, res) => {
-    res.send('Hello from Backend!')
-  })
+    res.send(bodyParser.json(req.body));
+    res.send('Hello from Backend!');
+  });
   
   app.listen(port, () => {
-    console.log(`App auf Port ${port} gestartet!`)
-  })
+    console.log(`App auf Port ${port} gestartet!`);
+  });
