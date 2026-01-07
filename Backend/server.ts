f8885c5b88d95e40132378036eede79507688ddf
pladde <mpoerschke@proton.me>
2026-01-07 07:53:55 +0100

    ankommende anfragen zu json verarbeitet, routen verknüpft

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,24 +1,13 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-
-//Import der Abhängigkeiten
-import { FahrradRepository } from './src/Repository/FahrradRepository.ts';
-import { FahrradService } from './src/Service/FahrradService.ts';
-import { FahrradController } from './src/Controller/FahrradController.ts';
-
-//Set-Up der Abhängigkeiten
-const fahrradRepository = new FahrradRepository();
-const fahrradService = new FahrradService(fahrradRepository);
-const fahrradController = new FahrradController(fahrradService);
+import rootRouter from './src/Routes/index.ts'
 
 const app = express();
 const port = 3000;
-
-app.post('/api', (req, res) => {
-    console.log(req.body);
-    res.send("hallo");
-  });
   
-  app.listen(port, () => {
+app.use(express.json());
+
+app.use('/api', rootRouter);
+
+app.listen(port, () => {
     console.log(`App auf Port ${port} gestartet!`);
   });
