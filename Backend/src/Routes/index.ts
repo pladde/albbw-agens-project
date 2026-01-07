225c418a3e71bc83dac192c8e497e322aecf2e72
pladde <mpoerschke@proton.me>
2026-01-07 07:55:14 +0100

    dependency injections hinzugefügt und fahrradRoute deklariert

--- Backend/src/Routes/index.ts
+++ Backend/src/Routes/index.ts
@@ -1,25 +1,16 @@
-import { Router } from "express";
+import { Router } from 'express';
+import fahrradRoutes from './FahrradRoutes';
 
-const router = Router();
+import { FahrradRepository } from '../Repository/FahrradRepository';
+import { FahrradService } from '../Service/FahrradService';;
+import { FahrradController } from '../Controller/FahrradController';
 
-router.post('/fahrrad/create', (req, res) => {
-    //controller.saveFahrrad(fahrrad);
-})
+const fahrradRepository = new FahrradRepository();
+const fahrradService = new FahrradService(fahrradRepository);
+const fahrradController = new FahrradController(fahrradService);
 
-router.get('/fahrrad/readById', (req, res) => {
-})
+const rootRouter = Router();
 
-router.get('/fahrrad/readByString', (req, res) => {
-})
+rootRouter.use('/fahrrad', fahrradRoutes(fahrradController))
 
-router.get('/fahrrad/readByDate', (req, res) => {
-})
-
-router.get('/fahrrad/readAll', (req, res) => {
-})
-
-router.put('/fahrrad/update', (req, res) => {
-})
-
-router.delete('/fahrrad/deleteById', (req, res) => {
-})
+export default rootRouter;
