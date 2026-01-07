4365b5864d973986ea5f37a2eec132585fe449e4
pladde <mpoerschke@proton.me>
2026-01-07 07:54:30 +0100

    werte optinal deklariert

--- Backend/src/Interfaces/IFahrrad.ts
+++ Backend/src/Interfaces/IFahrrad.ts
@@ -1,12 +1,13 @@
 export interface IFahrrad 
 {
     // Attribute
-    id: number;
-    marke: string;
-    rahmennummer: string;
-    besonderheiten: string;
+    id?: number;
+    marke?: string;
+    rahmennummer?: string;
+    besonderheiten?: string;
     bearbeitungsstatus: string;
     erfasstAm: Date;
     erfasstVon: string;
-    herausgegebenAn: string;
+    herausgegebenAn?: string;
+    qrCode: string;
 }
