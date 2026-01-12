4ae088d0afe4462d338a5108ad6c75ec617df488
pladde <mpoerschke@proton.me>
2026-01-12 14:54:15 +0100

    Routen implementiert

--- Backend/src/Routes/FahrradRoutes.ts
+++ Backend/src/Routes/FahrradRoutes.ts
@@ -7,5 +7,15 @@ export default function(fahrradController: FahrradController) {
 
     router.post('/create', (req, res) => fahrradController.saveFahrrad(req, res));
 
+    router.get('/getByNumber', (req, res) => fahrradController.findFahrradById(req, res));
+
+    router.get('/getByString', (req, res) => fahrradController.findFahrradByString(req, res));
+
+    router.get('/getByDate', (req, res) => fahrradController.findFahrradByDate(req, res));
+
+    router.get('/getAll', (req, res) => fahrradController.findAllFahrraeder(res));
+
+    router.delete('/deleteById', (req, res) => fahrradController.deleteFahrradById(req, res));
+
     return router;
 }
