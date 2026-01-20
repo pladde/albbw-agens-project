317626c953db6483c5562e698a11bb54b9af8a71
pladde <mpoerschke@proton.me>
2026-01-20 11:48:18 +0100

    fahrradService für die Dependecy Injection hinzugefügt

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AddFahrrad } from "./pages/AddFahrrad";
 import { SearchFahrrad } from "./pages/SearchFahrrad";
 import { Home } from "./pages/Home";
+import { fahrradService } from './services/fahrradService';
 
 
 function App() 
