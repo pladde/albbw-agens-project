731ff3d78d6362d8c001d89cabbcd0dd164a816d
pladde <mpoerschke@proton.me>
2026-01-20 11:48:33 +0100

    funktionales UI erstellt.

--- Frontend/src/pages/AddFahrrad.tsx
+++ Frontend/src/pages/AddFahrrad.tsx
@@ -1,41 +1,62 @@
-// Screen 2 Fahrrad hinzufügen
+// Screen 2
 
-import { useState } from "react";
+import {useState} from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
 
 export const AddFahrrad = () => {
+
     const [marke, setMarke] = useState<string>('');
     const [rahmennummer, setRahmennummer] = useState<string>('');
     const [besonderheiten, setBesonderheiten] = useState<string>('');
-
-    const handeSubmit = (e:React.FormEvent) => {
-        e.preventDefault();
-        console.log("Gespeichert wird: ", marke, rahmennummer, besonderheiten);
-        // BACKENDAUFRUF FOLGT
-    };
+    const [bearbeitungsstatus, setBearbeitungsstatus] = useState<string>('');
 
     return (
-        <Container className="mt-4">
-            <h3>Fahrrad erfassen</h3>
+        <Container className='mt-4'>
+            <Form className='mb-3'>
+                <Form.Label>Marke:</Form.Label>
+                <Form.Control 
+                    type='text' 
+                    placeholder='z.B. Canyon'
+                    value={marke}
+                    onChange={(e) => setMarke(e.target.value)} />
+            </Form>
 
-            <Form onSubmit={handeSubmit}>
-                <Form.Group className="mb-3">
-                    <Form.Label>Marke:</Form.Label>
-                    <Form.Control type="text" placeholder="z.B. Canyon" value={marke} onChange={(e) => setMarke(e.target.value)}/>
-                </Form.Group>
+            <Form className='mb-3'>
+                <Form.Label>Rahmenummer:</Form.Label>
+                <Form.Control 
+                    type='text' 
+                    placeholder='z.B. RH-0815'
+                    value={rahmennummer}
+                    onChange={(e) => setRahmennummer(e.target.value)} />
+            </Form>
 
-                <Form.Group className="mb-3">
-                    <Form.Label>Rahmennummer:</Form.Label>
-                    <Form.Control type="text" placeholder="z.B. RH-0815" value={rahmennummer} onChange={(e) => setRahmennummer(e.target.value)}/>
-                </Form.Group>
+            <Form className='mb-3'>
+                <Form.Label>Besonderheiten:</Form.Label>
+                <Form.Control 
+                    type='text'
+                    placeholder='z.B. Aerolenkeraufsatz'
+                    value={besonderheiten}
+                    onChange={(e) => setBesonderheiten(e.target.value)} /> 
+            </Form>
 
-                <Form.Group className="mb-3">
-                    <Form.Label>Besonderheiten:</Form.Label>
-                    <Form.Control type="text" placeholder="z.B. Narbendynamo" value={besonderheiten} onChange={(e) => setBesonderheiten(e.target.value)}/>
-                </Form.Group>
+            <Form.Group className='mb-3'>
+            <Form.Label>Bearbeitungsstatus:</Form.Label>
+                <Form.Select
+                    value={bearbeitungsstatus}
+                    onChange={(e) => setBearbeitungsstatus(e.target.value)}>
 
-                <Button variant="primary" type="submit">senden</Button>
+                    <option value="" disabled hidden>Status wählen...</option>
+                    <option value='1'>angenommen</option>
+                    <option value='2'>in Bearbeitung</option>
+                    <option value='3'>verfügbar</option>
+                    <option value='4'>herausgegeben</option>
+                </Form.Select>
+            </Form.Group>
+
+            <Form className='mb-3'>
+                <Button>senden</Button>
             </Form>
         </Container>
     );
 }
+// Später werde ich noch den Kunden hinzufügen, an dem das Fahrrad geht. Zu Testzwecken wird das noch nicht implementiert.
