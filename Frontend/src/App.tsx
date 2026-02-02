c6f8c838f604720709602ff39b8ced1e1d6ada30
pladde <mpoerschke@proton.me>
2026-02-02 14:29:04 +0100

    Header in den Router eingebunden. Der header wird nun vor jeder Route geladen.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,21 +1,19 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AddFahrrad } from './pages/AddFahrrad';
 import { SearchFahrrad } from './pages/SearchFahrrad';
 import { Home } from './pages/Home';
-// import { fahrradService } from './services/fahrradService';
-// import { Header } from './components/Header';
-
+import { Header } from './components/Header';
 
 function App() 
 {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/erfassen' element={<AddFahrrad />} />
-        <Route path='/suchen' element={<SearchFahrrad />} />
-      </Routes>
+    <Router >
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/erfassen' element={<AddFahrrad />} />
+          <Route path='/suchen' element={<SearchFahrrad />} />
+        </Routes>
     </Router>
   );
 }
