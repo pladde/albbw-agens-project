b5e6ef2f747fb16d1c8814e1b99d301fe85cbc6b
pladde <mpoerschke@proton.me>
2026-02-06 14:44:53 +0100

    IFahrrad mit Record ersetzt

--- Frontend/src/services/fahrradService.ts
+++ Frontend/src/services/fahrradService.ts
@@ -1,4 +1,4 @@
-import type { IFahrrad } from "../types/IFahrrad";
+//import type { IFahrrad } from "../types/IFahrrad";
 
 const API_URL = 'http://localhost:3000/api/fahrrad';
 
@@ -9,10 +9,10 @@ const API_URL = 'http://localhost:3000/api/fahrrad';
 export const fahrradService = {
     /**
      * Sendet eine Anfrage an das Backend um ein **neues Fahrrad-Objekt** zu **erstellen**
-     * @param {IFahrrad} data - Das vollständig ausgefüllte Fahrrad-Objekt.
-     * @returns {Promise<IFahrrad | null>} Das vom Server erstellte Objekt (inkl. generierter ID) oder null bei Fehler.
+     * @param {Record<string, any>} data - Das vollständig ausgefüllte Fahrrad-Objekt.
+     * @returns {Promise<Record<string, any> | null>} Das vom Server erstellte Objekt (inkl. generierter ID) oder null bei Fehler.
      */
-    create: async (data: IFahrrad): Promise<IFahrrad | null> => {
+    create: async (data: Record<string, any>): Promise<Record<string, any> | null> => {
         try {
             const response = await fetch(`${API_URL}`, {
                 method: 'POST',
@@ -41,7 +41,7 @@ export const fahrradService = {
             const response = await fetch(`${API_URL}/th`);
             if(!response.ok)
             {
-                throw new Error('Netzwerk-Fehler (fetchAll)');
+                throw new Error('Netzwerk-Fehler (fetchHead)');
             }
 
             return await response.json();
@@ -55,9 +55,9 @@ export const fahrradService = {
 
     /**
      * Ruft **alle** vorhandenen **Fahrräder** aus der Datenbank ab.
-     * @returns {Promise<IFahrrad[] | null>} Ein Array aller Fahrräder oder `null` bei Fehler.
+     * @returns {Promise<Record<string, any>[] | null>} Ein Array aller Fahrräder oder `null` bei Fehler.
      */
-    fetchAll: async (): Promise<IFahrrad[] | null> => {
+    fetchAll: async (): Promise<Record<string, any>[] | null> => {
         try
         {
             const response = await fetch(`${API_URL}`);
@@ -78,9 +78,9 @@ export const fahrradService = {
     /**
      * **Sucht** ein spezifisches Fahrrad-Objekt anhand der **ID** aus der Datenbank.
      * @param {number} id Der PK des Fahrrads
-     * @returns {Promise<IFahrrad | null>} Das gefundene Fahrrad oder `null` bei Fehler.
+     * @returns {Promise<Record<string, any> | null>} Das gefundene Fahrrad oder `null` bei Fehler.
      */
-    fetchById: async (id: number): Promise<IFahrrad | null> => {
+    fetchById: async (id: number): Promise<Record<string, any> | null> => {
         try {
             const response = await fetch(`${API_URL}/id/${id}`, { method: 'GET' });
 
@@ -102,13 +102,13 @@ export const fahrradService = {
      * **Sucht** nach **einem oder mehreren Fahrrädern** anhand eines **Strings**. 
      * @param {string} col Die Tabellenspalte, in der gesucht wird. (z.B. marke)
      * @param {string} val Der Wert nach dem gesucht wird. (z.B. Canyon)
-     * @returns {Promise<IFahrrad | null>} Das gefundene Fahrrad oder `null` bei Fehler.
+     * @returns {Promise<Record<string, any> | null>} Das gefundene Fahrrad oder `null` bei Fehler.
      */
-    fetchByString: async (col: string, val: string): Promise<IFahrrad[] | null> => {
+    fetchByString: async (col: string, val: string): Promise<Record<string, any> | null> => {
 
         try
         {
-            const response = await fetch(`${API_URL}/string/${col}&${val}`, { method: 'GET' });
+            const response = await fetch(`${API_URL}/string/${col}/${val}`, { method: 'GET' });
 
             if (!response.ok) 
             {
@@ -128,13 +128,13 @@ export const fahrradService = {
      * **Sucht** nach **einem oder mehreren Fahrrädern** anhand eines spezifischen Datums.
      * @param {string} col - DIe Spalte in der gesucht werden soll. (z.B. eingangsdatum)
      * @param {Date} val - Das Datum nach dem gesucht werden soll. (z.B. YYYY-MM-DD HH:MM:SS) 
-     * @returns {Promise<IFahrrad[] | null>} Gibt das Array mit den gefundenen Fahrrädern oder `null` bei Fehler zurück.
+     * @returns {Promise<Record<string, any> | null>} Gibt das Array mit den gefundenen Fahrrädern oder `null` bei Fehler zurück.
      */
-    fetchByDate: async (col: string, val: Date): Promise<IFahrrad[] | null> => {
+    fetchByDate: async (col: string, val: Date): Promise<Record<string, any> | null> => {
 
         try
         {
-            const response = await fetch(`${API_URL}/date/${col}&${val}`,{ method: 'GET' });
+            const response = await fetch(`${API_URL}/date/${col}/${val}`,{ method: 'GET' });
 
             if (!response.ok)
             {
@@ -153,9 +153,9 @@ export const fahrradService = {
     /**
      * **Löscht** ein **Fahrrad** anhand seiner **ID** aus der Datenbank.
      * @param {number} id - Der PK des Fahrrads.
-     * @returns {Promise<IFahrrad | null>} Gibt das gelöscht Fahrrad-Objekt oder `null` bei Fehler zurück.
+     * @returns {Promise<Record<string, any> | null>} Gibt das gelöscht Fahrrad-Objekt oder `null` bei Fehler zurück.
      */
-    deleteById: async (id: number): Promise<IFahrrad | null> => {
+    deleteById: async (id: number): Promise<Record<string, any> | null> => {
 
         try
         {
@@ -182,13 +182,13 @@ export const fahrradService = {
      * @param {number} id - Der Primary Key des Datensatzes.
      * @param {string} col - Die Spalte, in welcher der Wert geändert werden soll. (z.B. marke)
      * @param {string} val - Das exakte Datenfeld, welches geändert werden soll. (z.B. Canyon -> Cube)
-     * @returns {Promise<IFahrrad | null>} Gibt den überarbeiteten Datensatz oder `null` bei Fehler zurück.
+     * @returns {Promise<Record<string, any> | null>} Gibt den überarbeiteten Datensatz oder `null` bei Fehler zurück.
      */
-    editById: async (id: number, col: string, val: string): Promise<IFahrrad | null> => {
+    editById: async (id: number, col: string, val: string): Promise<Record<string, any> | null> => {
 
         try 
         {
-            const response = await fetch(`${API_URL}/${id}&${col}&${val}`, { method: 'PUT' });
+            const response = await fetch(`${API_URL}/${id}/${col}/${val}`, { method: 'PUT' });
 
             if (!response.ok)
             {
