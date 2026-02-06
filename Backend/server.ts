6b73325484d5abec5c3cda793fc724f286fcc0a4
pladde <mpoerschke@proton.me>
2026-02-06 08:53:43 +0100

    CORS zu Entwicklungszwecken hinzugefügt

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import rootRouter from './src/Routes/index.ts'
+import rootRouter from './src/Routes/index.ts';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
   
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api', rootRouter);
 
